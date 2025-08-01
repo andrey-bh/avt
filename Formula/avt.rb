2f54b2f774e96a0e2b0ba5b5c6854cb3f1359319
andrey-bh <andrey.bh@gmail.com>
2025-08-01 19:43:21 +0000

    Update Homebrew formula for version

--- Formula/avt.rb
+++ Formula/avt.rb
@@ -1,25 +1,25 @@
 class Avt < Formula
   desc "Video Tools by ARAKL"
   homepage "https://github.com/andrey-bh/avt"
-  version "1.0.0"
+  version "1.0.1"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/andrey-bh/avt/releases/download/1.0.0/avt-darwin-arm64.tar.gz"
-      sha256 "..." # darwin_arm64
+      url "https://github.com/andrey-bh/avt/releases/download/1.0.1/avt-darwin-arm64.tar.gz"
+      sha256 "60aa70e32adb4c788419add3e231c4e4c0a8bdc969e50a7122e5ce5a96f1e753" # darwin_arm64
     else
-      url "https://github.com/andrey-bh/avt/releases/download/1.0.0/avt-darwin-amd64.tar.gz"
-      sha256 "..." # darwin_amd64
+      url "https://github.com/andrey-bh/avt/releases/download/1.0.1/avt-darwin-amd64.tar.gz"
+      sha256 "178dc59c5f4af7b933c3dddcd72a033d14d6bcb0df0c85358f2d46040704e6f4" # darwin_amd64
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/andrey-bh/avt/releases/download/1.0.0/avt-linux-arm64.tar.gz"
-      sha256 "..." # linux_arm64
+      url "https://github.com/andrey-bh/avt/releases/download/1.0.1/avt-linux-arm64.tar.gz"
+      sha256 "9b68324b73bc7d33a163c185fe96bd1a74175aa40fcd0e4bd0d99be4e12fe4fe" # linux_arm64
     else
-      url "https://github.com/andrey-bh/avt/releases/download/1.0.0/avt-linux-amd64.tar.gz"
-      sha256 "..." # linux_amd64
+      url "https://github.com/andrey-bh/avt/releases/download/1.0.1/avt-linux-amd64.tar.gz"
+      sha256 "2148ada714cc85fb6f551345d1c78195e63b7ea36ad8b98e331572b15f252774" # linux_amd64
     end
   end
 
